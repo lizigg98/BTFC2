2b830fd99506e98443837a345fb8d06dc27a9e74
lizigg98 <liputh2003@gmail.com>
2023-10-02 17:08:34 +0700

    BT

---
+++ main/vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    // Set lại giá trị thời gian sau 5s
+    const timerId = setTimeout(() => {
+      setTime(5);
+    }, 5000);
+
+    return () => {
+      clearTimeout(timerId); // Xóa timeout nếu component unmount
+    };
+  }, []); // Chỉ chạy một lần khi component mount
+
+  return <div>{time}</div>;
+}
+
+export default App;
